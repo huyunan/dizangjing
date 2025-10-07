e2d172175864d0ca1df1cbd44b8eee65dfbce3ab
huyunan <18842920976@163.com>
2025-10-07 15:08:54 +0800

    地藏经 init

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,12 @@
+/**
+ * @see https://theme-plume.vuejs.press/config/navigation/ 查看文档了解配置详情
+ *
+ * Navbar 配置文件，它在 `.vuepress/plume.config.ts` 中被导入。
+ */
+
+import { defineNavbarConfig } from 'vuepress-theme-plume'
+
+export default defineNavbarConfig([
+  { text: '地藏经全文', link: '/' },
+  { text: '地藏经原文', link: '/notes/source/1.忉利天宫神通品第一.md' },
+])
