63fc841fcb50422774059a381d2cdf3ec6633866
huyunan <18842920976@163.com>
2026-05-16 10:45:48 +0800

    添加路由记忆功能

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,23 +1,20 @@
 import { defineClientConfig } from 'vuepress/client'
-// import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
-// import NpmBadge from 'vuepress-theme-plume/features/NpmBadge.vue'
-// import NpmBadgeGroup from 'vuepress-theme-plume/features/NpmBadgeGroup.vue'
-// import Swiper from 'vuepress-theme-plume/features/Swiper.vue'
-
-// import CustomComponent from './theme/components/Custom.vue'
-
 import './theme/styles/index.css'
 // import './theme/styles/custom.css'
 
 export default defineClientConfig({
-  enhance({ app }) {
-    // built-in components
-    // app.component('RepoCard', RepoCard)
-    // app.component('NpmBadge', NpmBadge)
-    // app.component('NpmBadgeGroup', NpmBadgeGroup)
-    // app.component('Swiper', Swiper) // you should install `swiper`
-
-    // your custom components
-    // app.component('CustomComponent', CustomComponent)
+  enhance({ app, router }) {
+    router.afterEach((to) => {
+      localStorage.setItem('to.path', to.fullPath);
+    });
+    router.beforeEach((to, from, next) => {
+      const topath = localStorage.getItem('to.path');
+      if (to.fullPath === '/' && from.fullPath === '/' && topath) {
+        router.push(topath);
+      }
+      next();
+    });
+    
+    
   },
 })
